b7ff575f3f7c68bedb848625216997bf30e9e095
biotecnologia2412-cmyk <biotecnologia2412@gmail.com>
2026-02-09 19:42:33 -0400

    Update entryID to new JotForm URL

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function finalizarJuego(resultado) {
     // resultado puede ser "Gano" o "Perdio"
     const formBaseURL = "https://docs.google.com";
-    const entryID = "entry.TU_ID_DE_PREGUNTA"; // El ID que anotaste antes
+    const entryID = "entry.https://form.jotform.com/260385621780055"; // El ID que anotaste antes
     
     // Redirigir al cliente al formulario con el resultado ya marcado
     window.location.href = `${formBaseURL}?usp=pp_url&${entryID}=${resultado}`;
