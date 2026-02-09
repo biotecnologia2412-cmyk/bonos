1b2c903f01e24c2b4b29729d817c145cd33e448f
biotecnologia2412-cmyk <biotecnologia2412@gmail.com>
2026-02-09 19:46:51 -0400

    Fix entryID declaration in finalizarJuego function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function finalizarJuego(resultado) {
     // resultado puede ser "Gano" o "Perdio"
     const formBaseURL = "https://docs.google.com";
-    const entryID = "entry.https://form.jotform.com/260385621780055"; // El ID que anotaste antes
+    const entryID = "entry.https://form.jotform.com/260385621780055";
     
     // Redirigir al cliente al formulario con el resultado ya marcado
     window.location.href = `${formBaseURL}?usp=pp_url&${entryID}=${resultado}`;
